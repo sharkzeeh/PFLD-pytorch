dee9b998c1e8b3161a92a57b1fc1844a2bfaa11c
Igor Babikov <igorbabikov24@gmail.com>
2020-12-24 14:40:11 +0300

    revamp PFLD; add yolov3; add euler angles; add custom weights download option; add quaternion model: model.pth; add option to load an image from web

---
+++ get_euler_angles.py
@@ -0,0 +1,75 @@
+import argparse
+import torch
+from torch import nn
+from torch.utils import data
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+from dataset.datasets import WLFWDatasets
+import torchvision.models as models
+from PIL import Image
+import os
+from scipy.spatial.transform import Rotation
+import detect
+import shutil
+
+opt = None
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+torch.cuda.empty_cache()
+
+def load_angle_model(path):
+    '''
+    Loads a pretrained model
+    '''
+    model = models.mobilenet_v2(num_classes=4)
+    model.load_state_dict(torch.load(path, map_location=device))
+    return model
+
+def quat_to_euler(vec):
+    '''
+    Transforms quaternion to euler angle (in degrees)
+    '''
+    rot = Rotation.from_quat(vec.cpu())
+    eulers = rot.as_euler('xyz', degrees=True)
+    return eulers
+
+def run_inference(opt=opt):
+    '''
+    Runs inference on a batch of images or a single image
+    '''
+    preprocess = transforms.Compose([transforms.ToTensor()])
+    if os.path.isdir(opt.cropped):
+        images = [os.path.join(opt.cropped, img) for img in os.listdir(opt.cropped)]
+    else:
+        images = [opt.cropped]
+    model.eval()
+    for img in images:
+        input_image = Image.open(img)
+        input_tensor = preprocess(input_image)
+        input_batch = input_tensor.unsqueeze(0)
+
+        if torch.cuda.is_available():
+            input_batch = input_batch.to('cuda')
+            model.to('cuda')
+
+        with torch.no_grad():
+            output = model(input_batch)
+
+        out = output[0]
+        eulers = quat_to_euler(out)
+        print(f"Image: {img.split('/')[-1]} has the following angles:\n Pitch: {eulers[0]:.2f}; Yaw: {eulers[1]:.2f}; Roll: {eulers[2]:.2f}\n")
+
+
+if __name__ == '__main__':
+    model = load_angle_model('./model.pth')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--source', type=str, default='./data/samples', help='source')  # file or directory
+    parser.add_argument('--cropped', type=str, default='./data/cropped', help='cropped images')  # file or directory
+    opt = parser.parse_args()
+    detect.run(source=opt.source)
+    run_inference(opt=opt)
+    if os.path.exists('./data/cropped'):
+        shutil.rmtree('./data/cropped')  # delete output folder
+    os.makedirs('./data/cropped')  # make new output folders
+
+    if os.path.exists('./data/web'):  # delete images downloaded from web if there are any
+        shutil.rmtree('./data/web')  # delete output folder
